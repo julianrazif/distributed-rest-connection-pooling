5f7d39b710cb9fb77230cc0f532c9e4349341de3
julian razif figaro <julian.razif@gmail.com>
2025-09-07 22:01:43 +0700

    Enhance DRCP configuration with custom request and cookie settings

--- distributed-rest-connection-pooling/src/main/java/com/julianrazif/module/drcp/DrcpAutoConfiguration.java
+++ distributed-rest-connection-pooling/src/main/java/com/julianrazif/module/drcp/DrcpAutoConfiguration.java
@@ -1,7 +1,10 @@
 package com.julianrazif.module.drcp;
 
 import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.config.TlsConfig;
+import org.apache.hc.client5.http.cookie.BasicCookieStore;
+import org.apache.hc.client5.http.cookie.StandardCookieSpec;
 import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
 import org.apache.hc.client5.http.impl.async.HttpAsyncClientBuilder;
 import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManager;
@@ -12,15 +15,19 @@ import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
 import org.apache.hc.core5.http.config.Http1Config;
 import org.apache.hc.core5.http2.HttpVersionPolicy;
 import org.apache.hc.core5.http2.config.H2Config;
+import org.apache.hc.core5.reactor.IOReactorConfig;
 import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 
 import java.net.ProxySelector;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 @AutoConfiguration
@@ -57,10 +64,32 @@ public class DrcpAutoConfiguration {
     return HttpAsyncClientBuilder.create()
       .setH2Config(H2Config.custom().setMaxConcurrentStreams(100).build())
       .setHttp1Config(Http1Config.DEFAULT)
+      .setDefaultRequestConfig(
+        RequestConfig.custom()
+          .setCookieSpec(StandardCookieSpec.STRICT)
+          .build()
+      )
+      .setDefaultCookieStore(new BasicCookieStore())
+      .setThreadFactory(workerThreadFactory())
+      .setIOReactorConfig(ioReactorConfig(Timeout.ofSeconds(35)))
       .setConnectionManager(connectionManager)
       .setRoutePlanner(new SystemDefaultRoutePlanner(ProxySelector.getDefault()))
       .disableRedirectHandling()
       .disableAutomaticRetries()
+      .build();
+  }
+
+  private ThreadFactory workerThreadFactory() {
+    CustomizableThreadFactory workerThreadFactory = new CustomizableThreadFactory("DRCP-worker-");
+    workerThreadFactory.setThreadPriority(Thread.MAX_PRIORITY);
+    workerThreadFactory.setDaemon(true);
+    return workerThreadFactory;
+  }
+
+  private IOReactorConfig ioReactorConfig(Timeout timeout) {
+    return IOReactorConfig.custom()
+      .setSoTimeout(timeout)
+      .setIoThreadCount(4)
       .build();
   }
 
